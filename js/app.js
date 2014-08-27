c11004311241b2c65b27ec8e7305bd1923548fe9
ddimitrov90 <m1tkoregs@gmail.com>
2014-08-27 11:45:51 +0300

    intial ng app

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('blogApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'blogApp.home'
+  //'myApp.filters',
+  //'myApp.services',
+  //'myApp.directives',
+  //'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
---
+++ js/home/homeApp.js
@@ -0,0 +1,7 @@
+(function(){
+	var homeApp = angular.module('blogApp.home',[]);
+
+	homeApp.controller('homeController',[function(){
+		this.test = 'mitkooo';
+	}]);
+})();

aebc761482f502b567b7a55bdc97b4035b7b2c8e
Dimitar Dimitrov <m1tkoregs@gmail.com>
2014-09-18 00:41:26 +0300

    implemented getblogposts/getblogpostbyurl/addnewpost

--- js/components/everlive-service/everlive-service.js
+++ js/components/everlive-service/everlive-service.js
@@ -1,19 +1,50 @@
 (function() {
     var blogAppServices = angular.module('blogApp.services', []);
-    blogAppServices.service('EverliveService', function($q){
-    	this.test = 'Mitkoo0o0';
-    	var el = new Everlive('fW5fEkhhplSXgaCS');
-    	var data= el.data('BlogPost');
+    blogAppServices.service('EverliveService', function($q) {
+        var el = new Everlive('fW5fEkhhplSXgaCS');
+        var data = el.data('BlogPost');
 
-    	this.getBlogPosts = function() {
-    		var deferred = $q.defer();
-    		data.get().then(function(data){
-		         deferred.resolve(data);
-		    },
-		    function(error){
-		        deferred.reject(error);
-		    });
-		    return deferred.promise;
-    	};
+        this.getBlogPosts = function getBlogPosts(count) {
+            if (!count) {
+                count = 5;
+            }
+            var query = new Everlive.Query();
+            query.order('Date').take(count);
+
+            var deferred = $q.defer();
+            data.get(query).then(function(data) { 
+                    deferred.resolve(data);
+                },
+                function(error) {
+                    deferred.reject(error);
+                });
+            return deferred.promise;
+        };
+
+        this.getBlogPostByUrl = function getBlogPostByUrl(url) {
+            var deferred = $q.defer();
+            data.get({
+                Url: url
+            }).then(function(data) { 
+                    deferred.resolve(data);
+                },
+                function(error) {
+                    deferred.reject(error);
+                });
+            return deferred.promise;
+        };
+
+        this.addNewBlogPost = function addNewBlogPost(blogPost) {
+            var deferred = $q.defer();
+            data.create(blogPost).then(function(data) { 
+                    deferred.resolve(data);
+                },
+                function(error) {
+                    deferred.reject(error);
+                });
+            return deferred.promise;
+        };
+
+
     });
 })();

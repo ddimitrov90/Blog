3319f73b1a3727d37608a2b01babd6701d2fb753
Dimitar Dimitrov <m1tkoregs@gmail.com>
2014-09-21 00:15:20 +0300

    refractor archive directive html added logic to build archive data in everlive-service

--- js/components/archive/archive.js
+++ js/components/archive/archive.js
@@ -1,11 +1,22 @@
 (function() {
 
-    var archiveApp = angular.module('blogApp.archive', []);
+    var archiveApp = angular.module('blogApp.archive', [ 'blogApp.services']);
+
+    archiveApp.controller('archiveWidgetController', function($scope, EverliveService) {
+       	EverliveService.getBlogPostsArchive().then(
+          function(result) { 
+                $scope.postsArchive = result;
+          },
+          function() {
+          }
+        );
+    });
 
     archiveApp.directive('archiveWidget', function() {
         return {
-        	restrict: 'E',
-            templateUrl: '/js/components/archive/archiveView.html'
+            restrict: 'E',
+            templateUrl: '/js/components/archive/archiveView.html',
+            controller: 'archiveWidgetController'
         };
     });
 })();
--- js/components/everlive-service/everlive-service.js
+++ js/components/everlive-service/everlive-service.js
@@ -1,6 +1,7 @@
 (function() {
     var blogAppServices = angular.module('blogApp.services', []);
     blogAppServices.service('EverliveService', function($q) {
+        var self = this;
         var el = new Everlive('fW5fEkhhplSXgaCS');
         var data = el.data('BlogPost');
 
@@ -57,5 +58,32 @@
                 });
             return deferred.promise;
         };
+
+        this.getBlogPostsArchive = function getBlogPostsArchive() {
+
+            var query = new Everlive.Query();
+            query.orderDesc('Date').select('Title', 'Url', 'Archive');
+
+            var deferred = $q.defer();
+            data.get(query).then(function(data) { 
+                    var archivedData = self._buildArchiveData(data.result);
+                    deferred.resolve(archivedData);
+                },
+                function(error) {
+                    deferred.reject(error);
+                });
+            return deferred.promise;
+        }
+
+        this._buildArchiveData = function _buildArchiveData(everliveData) {
+            var result = {};
+            for (var i = 0; i < everliveData.length; i++) {
+                var currentBlogPost = everliveData[i];
+                if(!result[currentBlogPost.Archive]){
+                    result[currentBlogPost.Archive] = [];
+                }
+                result[currentBlogPost.Archive].push(currentBlogPost);
+            };
+        }
     });
 })();

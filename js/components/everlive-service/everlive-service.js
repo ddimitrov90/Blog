315b4c7cdf0ee317d58691a3b8197207e4c25c2b
Dimitar Dimitrov <m1tkoregs@gmail.com>
2014-09-21 15:16:47 +0300

    tag cloud widget sets the correct css class

--- js/components/everlive-service/everlive-service.js
+++ js/components/everlive-service/everlive-service.js
@@ -100,20 +100,42 @@
                 result[currentBlogPost.Archive].push(currentBlogPost);
             };
             return result;
-        }
+        };
 
         this.getTags = function getTags() {
             var query = new Everlive.Query();
             query.orderDesc('Counter');
 
             var deferred = $q.defer();
-            tagsData.get(query).then(function(data) { 
-                    deferred.resolve(data.result);
+            tagsData.get().then(function(data) { 
+                    var tags = self._buildTagsCss(data.result);
+                    deferred.resolve(tags);
                 },
                 function(error) {
                     deferred.reject(error);
                 });
             return deferred.promise;
+        };
+
+        this._buildTagsCss = function _buildTagsCss(everliveData) {
+            var result = [];
+            for (var i = 0; i < everliveData.length; i++) {
+                var tag = everliveData[i];
+                if (tag.Counter < 10) {
+                    result.push({
+                        Name: tag.Name,
+                        CssClass: 'tag1'
+                    });
+                } else {
+                    var size = Math.round(tag.Counter / 10);
+                    result.push({
+                        Name: tag.Name,
+                        CssClass: 'tag' + size
+                    })
+                }
+            };
+            return result;
+
         };
     });
 })();

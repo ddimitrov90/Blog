a00eeca4fb8081101bfdb92906c7ed19f61637d5
Dimitar Dimitrov <m1tkoregs@gmail.com>
2015-02-25 23:18:14 +0200

    small fixes

--- js/components/everlive-service/everlive-service.js
+++ js/components/everlive-service/everlive-service.js
@@ -2,7 +2,7 @@
     var blogAppServices = angular.module('blogApp.services', []);
     blogAppServices.service('EverliveService', function($q) {
         var self = this;
-        var el = new Everlive('fW5fEkhhplSXgaCS');
+        var el = new Everlive('uYpwy6tnxBpJenHo');
         var blogPostData = el.data('BlogPost');
         var tagsData = el.data('Tags');
 
